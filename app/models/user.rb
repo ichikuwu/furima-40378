cc1a1e9d7f6f273fb8e86e07814894f58b7c06c0
ichikuwu <q08012@gmail.com>
2024-03-22 14:36:52 +0900

    ログイン・ログアウト機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+
+
+  validates :nickname, presence: true, length: { maximum: 6 }
+  validates :last_name,presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :first_name,presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :last_name_kana,presence: true, format: { with: /\A[ァ-ヶー]/ }
+  validates :first_name_kana,presence: true, format: { with: /\A[ァ-ヶー]/ }
+  validates :birthday,presence: true
+
+
 end
