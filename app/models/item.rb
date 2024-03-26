d62acdfb4819bd08139b1a2ab7d3c5ed503a05cd
ichikuwu <q08012@gmail.com>
2024-03-26 20:11:42 +0900

    バリデーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,9 +11,14 @@ class Item < ApplicationRecord
   belongs_to :ship_days
   has_one_attached :image
 
-  validates :category, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :item_condition,presence: true, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :ship_type,presence: true, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :region,presence: true, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :ship_days,presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :image,          presence: true
+  validates :item_name,      presence: true
+  validates :item_memo,      presence: true
+  validates :category,       presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :item_condition, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :ship_typ,       presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :region,         presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :ship_days,      presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :item_price,     presence: true
+
 end
