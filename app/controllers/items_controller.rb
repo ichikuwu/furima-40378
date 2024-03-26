f39f61fbc498f028eae8a729e85260d641e3c585
ichikuwu <q08012@gmail.com>
2024-03-26 19:48:57 +0900

    ActiveHashを導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,4 +4,13 @@ class ItemsController < ApplicationController
     #@itemds = item.all
   end
 
+  def new
+    #@items = item.new
+  end
+
+   def create
+    #item.create(item_params)
+    #redirect_to '/'
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,19 +1,19 @@
 class Item < ApplicationRecord
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
 
   has_one     :order
   belongs_to  :user
-  belongs_to_active_hash :category
-  belongs_to_active_hash :item_condition
-  belongs_to_active_hash :ship_type
-  belongs_to_active_hash :region
-  belongs_to_active_hash :ship_days
-
+  belongs_to :category
+  belongs_to :item_condition
+  belongs_to :ship_type
+  belongs_to :region
+  belongs_to :ship_days
   has_one_attached :image
 
-  validates :category, presence: true
-  validates :item_condition,presence: true
-  validates :ship_type,presence: true
-  validates :region,presence: true
-  validates :ship_days,presence: true
+  validates :category, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :item_condition,presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :ship_type,presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :region,presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :ship_days,presence: true, numericality: { other_than: 1 , message: "can't be blank"}
 end
