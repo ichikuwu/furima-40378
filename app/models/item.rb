02fcc0e816bf4d7e1aa769039e881434c2ba6fc4
ichikuwu <q08012@gmail.com>
2024-03-26 17:05:46 +0900

    ActiveStorageを導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,19 @@
 class Item < ApplicationRecord
+
+
+  has_one     :order
+  belongs_to  :user
+  belongs_to_active_hash :category
+  belongs_to_active_hash :item_condition
+  belongs_to_active_hash :ship_type
+  belongs_to_active_hash :region
+  belongs_to_active_hash :ship_days
+
+  has_one_attached :image
+
+  validates :category, presence: true
+  validates :item_condition,presence: true
+  validates :ship_type,presence: true
+  validates :region,presence: true
+  validates :ship_days,presence: true
 end
